16da22a648e68931cb2f2a733a17a0d6a98bf455
Kai Hugo Hustoft Endresen <kai.endresen@gmail.com>
2015-03-25 06:37:53 +0100

    added program options (needs modification)||

--- SensorConverter/src/main.cpp
+++ SensorConverter/src/main.cpp
@@ -4,11 +4,44 @@
 #include <rectification.h>
 #include <processthread.h>
 #include <opencv2/opencv.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 using namespace std;
 
-int main()
+int main( int argc, char** argv )
 {
+    po::options_description desc;
+
+    std::string output_name;
+    int cameras;
+
+    desc.add_options()
+            ( "v4l2", "Use V4L2" )
+            ( "ximea", "Use XIMEA" )
+            ( "cameras", po::value<int>(&cameras)->default_value(0), "cameras" )
+            ( "output_name",po::value<std::string>(&output_name)->default_value("XI_IMG"), "Output Group Name" );
+
+    po::variables_map vm;
+
+    try
+    {
+        po::store( po::parse_command_line( argc, argv, desc ), vm );
+        po::notify( vm );
+    }
+    catch( const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    if( vm.count( "output_name" ) )
+    {
+        std::cout << "Using: " << output_name << std::endl;
+    }
+
     std::cout << "threads: " << cv::getNumThreads() << std::endl;
     sepia::Reader input( "XI_IMG" );
     sepia::Writer output( "RGB_RECTIFIED", input.getGroupHeader()->count, 1280, 1024, 24 );
